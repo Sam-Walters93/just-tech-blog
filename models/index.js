227e7e7ea778ae3d52829d7be8aac9e659a8c91f
Sam Walters <stwalters314@gmail.com>
2021-05-23 11:51:20 -0500

    created comment model and set routes

--- models/index.js
+++ models/index.js
@@ -1,7 +1,6 @@
 const User = require("./User");
 const Post = require("./Post");
-
-module.exports = { User, Post };
+const Comment = require("./Comment");
 
 User.hasMany(Post, {
     foreignKey: 'user_id'
@@ -10,3 +9,21 @@ User.hasMany(Post, {
 Post.belongsTo(User, {
     foreignKey: 'user_id'
 });
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+  
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+  
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+  
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = module.exports = { User, Post, Comment };
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,17 +1,30 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 
 router.get('/', (req, res) => {
-    Post.findAll({
-        attributes: [ 'id', 'post_url', 'title', 'created_at' ],
-        order: [['created_at', 'DESC']], 
-        include: [
-            {
-                model: User, 
-                attributes: ['username']
-            }
-        ]
-    })
+  Post.findAll({
+    order: [['created_at', 'DESC']],
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+   })
     .then(dbPostData => res.json(dbPostData))
     .catch(err => {
         console.log(err);
